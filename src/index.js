aaf7099e9b2c63603b63286506b575ccebb7ae3c
BadKoldrysh <ohmygodkir17@gmail.com>
2019-07-12 19:38:08 +0200

    Modify main method with adding container and a few variables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+
+
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
@@ -6,9 +8,12 @@ import "./index.css";
 
 class App extends Component {
   render() {
-      const heading = <h1 className="site-heading">Hello, React World!</h1>
+    const name = "Jack";
+    const heading = <h1 className="site-heading">Hello, React World!</h1>;
+    const helloName = <h3 className="other-class">Hello, {name}</h3>;
+    const container = <div>{heading}{helloName}</div>
     return (
-      heading
+      container
     );
   }
 }
